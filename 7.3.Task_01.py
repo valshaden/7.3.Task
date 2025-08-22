96591d27ef21530f3eb566934c43dcd3fe528481
valshaden <valshaden@yandex.ru>
2025-08-22 23:32:21 +0300

    + file task_01

---
+++ 7.3.Task_01.py
@@ -0,0 +1,21 @@
+# 7.3. Задание
+## Доработайте проект, который генерирует изображение с помощью ИИ. 
+## Добавьте в проект интерфейс tkinter. Пользователь должен вводить запрос 
+## в поле ввода и нажимать на кнопку "Отправить". Изображение, сгенерированное 
+## по ссылке, должно открываться в отдельном вторичном окне. 
+
+# Код проекта:
+
+import asyncio
+from g4f.client import AsyncClient
+async def main():
+    client = AsyncClient()
+    response = await client.images.generate(
+        prompt="a white siamese cat",
+        model="flux",
+        response_format="url"
+        # Add any other necessary parameters
+    )
+    image_url = response.data[0].url
+    print(f"Generated image URL: {image_url}")
+asyncio.run(main())
