969d0a205aca53cbcf57574d36b19f7577f3fdfd
valshaden <valshaden@yandex.ru>
2025-08-24 13:57:38 +0300

    + file cats.py

--- cats.py
+++ cats.py
@@ -1,3 +1,5 @@
+# Добавляем загрузку по тегу
+
 from tkinter import *
 from PIL import Image, ImageTk
 import requests
@@ -18,47 +20,48 @@ def load_image(url):
 def open_new_window():
     tag = tag_entry.get()
     url_with_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    print(f"URL с тегом: {url_with_tag}")
     img = load_image(url_with_tag)
     
     if img:
         new_window = Toplevel()
-        new_window.title("Cat Image")
+        ##new_window.title("Cat Image")
+        new_window.title(tag)
         new_window.geometry("600x480")
         label = Label(new_window, image=img)
         label.image = img
         label.pack()
 
-
-def exit_app():
-    window.destroy()
+##def exit_app():
+##    window.destroy()
 
 window = Tk()
-window.title("Cats!")
-window.geometry("600x520")
+window.title("Внимание. Работает ИИ!")
+window.geometry("480x320")
 
 label = Label(window)
 label.pack()
 
-# Создаем меню
-menu_bar = Menu(window)
-window.config(menu=menu_bar)
+### Создаем меню
+##menu_bar = Menu(window)
+##window.config(menu=menu_bar)
 
-# Добавляем пункты меню
-file_menu = Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label="Файл", menu=file_menu)
+### Добавляем пункты меню
+##file_menu = Menu(menu_bar, tearoff=0)
+##menu_bar.add_cascade(label="Файл", menu=file_menu)
 # Каждая картинка в отдельном окне
 #file_menu.add_command(label="Загрузить фото", command=set_image)
-file_menu.add_command(label="Загрузить фото", command=open_new_window)
+##file_menu.add_command(label="Загрузить фото", command=open_new_window)
 
-file_menu.add_separator()
-file_menu.add_command(label="Выход", command=exit_app)
+##file_menu.add_separator()
+##file_menu.add_command(label="Выход", command=exit_app)
 
 # Поле ввода для тегов
 tag_entry = Entry()
 tag_entry.pack()
 
 # Кнопка для загрузки изображения с тегом
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button = Button(text="Создать изображение", command=open_new_window)
 load_button.pack()
 
 window.mainloop()
